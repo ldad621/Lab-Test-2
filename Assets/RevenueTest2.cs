c9d6eecbad7943296156a57f1e11951f204896c8
ldad621 <ldad621@aucklanduni.ac.nz>
2017-06-08 18:36:16 +1200

    Getting Revenue Azure Table connected to Unity
    beginning

---
+++ Assets/RevenueTest2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class RevenueTest2
+{
+	public string ProductID { get; set; }
+	public string Date { get; set; }
+	public string Units { get; set; }
+	public string Revenue { get; set; }
+	public string City { get; set; }
+	public string State { get; set; }
+	public string Category { get; set; }
+	public string IsCompete { get; set; }
+	public string Segment { get; set; }
+	public int X { get; set; }
+	public int Y { get; set; }
+	public int Z { get; set; }
+}
+
