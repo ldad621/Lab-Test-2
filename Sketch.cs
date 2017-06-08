6b70b353265f1ef92834b54ecc5f1989daad71a5
ldad621 <ldad621@aucklanduni.ac.nz>
2017-06-08 18:51:46 +1200

    Creating a cube for each row in RevenueTest2

---
+++ Sketch.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using Pathfinding.Serialization.JsonFx; //make sure you include this using
+
+public class Sketch : MonoBehaviour {
+    public GameObject myPrefab;
+
+	string _WebsiteURL = "http://infomgmt192.azurewebsites.net/tables/RevenueTest2?zumo-api-version=2.0.0";
+
+    void Start () {
+	    string jsonResponse = Request.GET(_WebsiteURL);
+        if (string.IsNullOrEmpty(jsonResponse))
+        {
+            return;
+        }
+		RevenueTest2[] rt2 = JsonReader.Deserialize<RevenueTest2[]>(jsonResponse);
+
+		int totalCubes = rt2.Length;
+        int totalDistance = 5;
+        int i = 0;
+
+		foreach (RevenueTest2 revenue in rt2)
+        {
+			Debug.Log("This products name is: " + revenue.City);
+            float perc = i / (float)totalCubes;
+            i++;
+            float x = perc * totalDistance;
+            float y = 5.0f;
+            float z = 0.0f;
+            GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            newCube.GetComponent<myCubeScript>().setSize((1.0f - perc) * 2);
+            newCube.GetComponent<myCubeScript>().ratateSpeed = perc;
+			//newCube.GetComponentInChildren<TextMesh>().text = revenue.City;
+
+        }
+    }
+	void Update () {
+	
+	}
+}
